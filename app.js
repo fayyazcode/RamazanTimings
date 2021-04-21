6f0ae18d55bc09b4ee194ae7e711d02d5afa67b4
Fayyaz Alam <fayazalam.98@gmail.com>
2021-04-21 02:19:42 -0700

    half work done

--- app.js
+++ app.js
@@ -13,3 +13,18 @@ else if(date==2){
 else if(date==3){
     document.write("<h3> Sehri </h3> 04:49 AM "+"<h3>Iftar </h3> : 6:56 PM "+"<h4>Date</h4> 16 April 2021")
 }
+else if(date==4){
+    document.write("<h3> Sehri </h3> 04:48 AM "+"<h3>Iftar </h3> : 6:56 PM "+"<h4>Date</h4> 17 April 2021")
+}
+else if(date==5){
+    document.write("<h3> Sehri </h3> 04:47 AM "+"<h3>Iftar </h3> : 6:57 PM "+"<h4>Date</h4> 18 April 2021")
+}
+else if(date==6){
+    document.write("<h3> Sehri </h3> 04:46 AM "+"<h3>Iftar </h3> : 6:57 PM "+"<h4>Date</h4> 19 April 2021")
+}
+else if(date==7){
+    document.write("<h3> Sehri </h3> 04:45 AM "+"<h3>Iftar </h3> : 6:57 PM "+"<h4>Date</h4> 20 April 2021")
+}
+else if(date==8){
+    document.write("<h3> Sehri </h3> 04:44 AM "+"<h3>Iftar </h3> : 6:58 PM "+"<h4>Date</h4> 21 April 2021")
+}
