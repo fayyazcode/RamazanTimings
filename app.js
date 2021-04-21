7b0deb784d5a582f922d584c49dcaf9ce562da89
Fayyaz Alam <fayazalam.98@gmail.com>
2021-04-21 02:18:42 -0700

    use ing if and else if

---
+++ app.js
@@ -0,0 +1,15 @@
+var date=+prompt("Enter date to know timings of iftari and sehri")
+
+// RAMADAN	SEHRI	IFTAR	DATE
+
+
+
+if(date==1){
+    document.write("<h3> Sehri </h3> 04:51 AM "+"<h3>Iftar </h3> : 6:55 PM "+"<h4>Date</h4> 14 April 2021")
+}
+else if(date==2){
+    document.write("<h3> Sehri </h3> 04:50 AM "+"<h3>Iftar </h3> : 6:55 PM "+"<h4>Date</h4> 15 April 2021")
+}
+else if(date==3){
+    document.write("<h3> Sehri </h3> 04:49 AM "+"<h3>Iftar </h3> : 6:56 PM "+"<h4>Date</h4> 16 April 2021")
+}
