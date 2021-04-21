8a054e936fa005aaee60e2df28655fe29065a3ba
Fayyaz Alam <fayazalam.98@gmail.com>
2021-04-21 02:20:14 -0700

    completed

--- app.js
+++ app.js
@@ -28,3 +28,72 @@ else if(date==7){
 else if(date==8){
     document.write("<h3> Sehri </h3> 04:44 AM "+"<h3>Iftar </h3> : 6:58 PM "+"<h4>Date</h4> 21 April 2021")
 }
+else if(date==9){
+    document.write("<h3> Sehri </h3> 04:43 AM "+"<h3>Iftar </h3> : 6:58 PM "+"<h4>Date</h4> 22 April 2021")
+}
+else if(date==10){
+    document.write("<h3> Sehri </h3> 04:42 AM "+"<h3>Iftar </h3> : 6:59 PM "+"<h4>Date</h4> 23 April 2021")
+}
+else if(date==11){
+    document.write("<h3> Sehri </h3> 04:41 AM "+"<h3>Iftar </h3> : 6:59 PM "+"<h4>Date</h4> 24 April 2021")
+}
+else if(date==12){
+    document.write("<h3> Sehri </h3> 04:40 AM "+"<h3>Iftar </h3> : 7:00 PM "+"<h4>Date</h4> 25 April 2021")
+}
+else if(date==13){
+    document.write("<h3> Sehri </h3> 04:39 AM "+"<h3>Iftar </h3> : 7:00 PM "+"<h4>Date</h4> 26 April 2021")
+}
+else if(date==14){
+    document.write("<h3> Sehri </h3> 04:38 AM "+"<h3>Iftar </h3> : 7:01 PM "+"<h4>Date</h4> 27 April 2021")
+}
+else if(date==15){
+    document.write("<h3> Sehri </h3> 04:37 AM "+"<h3>Iftar </h3> : 7:01 PM "+"<h4>Date</h4> 28 April 2021")
+}
+else if(date==16){
+    document.write("<h3> Sehri </h3> 04:36 AM "+"<h3>Iftar </h3> : 7:02 PM "+"<h4>Date</h4> 29 April 2021")
+}
+else if(date==17){
+    document.write("<h3> Sehri </h3> 04:35 AM "+"<h3>Iftar </h3> : 7:02 PM "+"<h4>Date</h4> 30 April 2021")
+}
+else if(date==18){
+    document.write("<h3> Sehri </h3> 04:34 AM "+"<h3>Iftar </h3> : 7:03 PM "+"<h4>Date</h4> 01 May 2021")
+}
+else if(date==19){
+    document.write("<h3> Sehri </h3> 04:33 AM "+"<h3>Iftar </h3> : 7:03 PM "+"<h4>Date</h4> 02 May 2021")
+}
+else if(date==20){
+    document.write("<h3> Sehri </h3> 04:32 AM "+"<h3>Iftar </h3> : 7:04 PM "+"<h4>Date</h4> 03 May 2021")
+}
+else if(date==21){
+    document.write("<h3> Sehri </h3> 04:31 AM "+"<h3>Iftar </h3> : 7:04 PM "+"<h4>Date</h4> 04 May 2021")
+}
+else if(date==22){
+    document.write("<h3> Sehri </h3> 04:30 AM "+"<h3>Iftar </h3> : 7:05 PM "+"<h4>Date</h4> 05 May 2021")
+}
+else if(date==23){
+    document.write("<h3> Sehri </h3> 04:29 AM "+"<h3>Iftar </h3> : 7:05 PM "+"<h4>Date</h4> 06 May 2021")
+}
+else if(date==24){
+    document.write("<h3> Sehri </h3> 04:28 AM "+"<h3>Iftar </h3> : 7:06 PM "+"<h4>Date</h4> 07 May 2021")
+}
+else if(date==25){
+    document.write("<h3> Sehri </h3> 04:28 AM "+"<h3>Iftar </h3> : 7:06 PM "+"<h4>Date</h4> 08 May 2021")
+}
+else if(date==26){
+    document.write("<h3> Sehri </h3> 04:27 AM "+"<h3>Iftar </h3> : 7:07 PM "+"<h4>Date</h4> 09 May 2021")
+}
+else if(date==27){
+    document.write("<h3> Sehri </h3> 04:26 AM "+"<h3>Iftar </h3> : 7:07 PM "+"<h4>Date</h4> 10 May 2021")
+}
+else if(date==28){
+    document.write("<h3> Sehri </h3> 04:25 AM "+"<h3>Iftar </h3> : 7:08 PM "+"<h4>Date</h4> 11 May 2021")
+}
+else if(date==29){
+    document.write("<h3> Sehri </h3> 04:24 AM "+"<h3>Iftar </h3> : 7:08 PM "+"<h4>Date</h4> 12 May 2021")
+}
+else if(date==30){
+    document.write("<h3> Sehri </h3> 04:24 AM "+"<h3>Iftar </h3> : 7:09 PM "+"<h4>Date</h4> 13 May 2021")
+}
+else{
+    alert("Invalid Number")
+}
